5f50026051c62f41e930593c1a8f38b61c1be2fd
Aviad Shikloshi <aviad@perimeterx.com>
2016-10-06 15:12:41 +0300

    debuging travis script

--- deploy/before-deploy.sh
+++ deploy/before-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+echo "in this director\n$(ls)"
 
 if [ "$TRAVIS_BRANCH" = 'buildAndDeployAutomation' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_89dd7f8061de_key -iv $encrypted_89dd7f8061de_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import deploy/signingkey.asc
+    gpg --fast-import deploy/codesigning.asc
 fi
