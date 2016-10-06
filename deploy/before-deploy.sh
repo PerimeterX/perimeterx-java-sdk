c19397d0f9bbcddb29c78320382895b74cf93962
Aviad Shikloshi <aviad@perimeterx.com>
2016-10-06 15:08:12 +0300

    update deploy script

--- deploy/before-deploy.sh
+++ deploy/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'buildAndDeployAutomation' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl openssl aes-256-cbc -K $encrypted_89dd7f8061de_key -iv $encrypted_89dd7f8061de_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_89dd7f8061de_key -iv $encrypted_89dd7f8061de_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import deploy/signingkey.asc
 fi
