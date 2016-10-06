05c856633ad629eea0349f57bf5fa5fbe1fcbd6a
Aviad Shikloshi <aviad@perimeterx.com>
2016-10-06 15:15:48 +0300

    debuging travis script

--- deploy/before-deploy.sh
+++ deploy/before-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-echo "in this director\n$(ls)"
 
 if [ "$TRAVIS_BRANCH" = 'buildAndDeployAutomation' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_89dd7f8061de_key -iv $encrypted_89dd7f8061de_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import deploy/codesigning.asc
+    openssl aes-256-cbc -K $encrypted_89dd7f8061de_key -iv $encrypted_89dd7f8061de_iv -in ./deploy/codesigning.asc.enc -out ./deploy/codesigning.asc -d
+    gpg --fast-import ./deploy/codesigning.asc
 fi
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ls
+
 if [ "$TRAVIS_BRANCH" = 'buildAndDeployAutomation' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P ossrh --settings deploy/mvnsettings.xml
 fi
