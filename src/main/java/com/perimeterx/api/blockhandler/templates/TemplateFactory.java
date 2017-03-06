348073655c3efdab13131659eee5d706dbe1cdf9
Aviad Shikloshi <aviad@perimeterx.com>
2017-03-06 09:26:53 +0200

    CR fixes

--- src/main/java/com/perimeterx/api/blockhandler/templates/TemplateFactory.java
+++ src/main/java/com/perimeterx/api/blockhandler/templates/TemplateFactory.java
@@ -6,21 +6,20 @@ import com.github.mustachejava.MustacheFactory;
 import com.perimeterx.api.PXConfiguration;
 import com.perimeterx.models.PXContext;
 import com.perimeterx.models.exceptions.PXException;
-import com.perimeterx.utils.Constants;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by nitzangoldfeder on 02/03/2017.
  * This class is a helper class, in order to get a template the factory receives a name of a template and returns
  * the template compiled
+ *
+ * Created by nitzangoldfeder on 02/03/2017.
  */
 public abstract class TemplateFactory {
 
@@ -29,7 +28,7 @@ public abstract class TemplateFactory {
             Map<String, String> props = getProps(pxContext, pxConfig);
 
             MustacheFactory mf = new DefaultMustacheFactory();
-            Mustache m = mf.compile((getTemplate(template)),(getTemplate(template)).toString());
+            Mustache m = mf.compile((getTemplate(template)), (getTemplate(template)).toString());
             StringWriter sw = new StringWriter();
             m.execute(sw, props).close();
             return sw.toString();
@@ -46,7 +45,7 @@ public abstract class TemplateFactory {
         props.put("refId", pxContext.getUuid());
         props.put("vid", pxContext.getVid());
         props.put("uuid", pxContext.getUuid());
-        props.put("customLogo",pxConfig.getCustomLogo());
+        props.put("customLogo", pxConfig.getCustomLogo());
         props.put("cssRef", pxConfig.getCssRef());
         props.put("jsRef", pxConfig.getJsRef());
         props.put("logoVisibility", pxConfig.getCustomLogo() == null ? "hidden" : "visible");
@@ -54,17 +53,13 @@ public abstract class TemplateFactory {
         return props;
     }
 
-    private static StringReader getTemplate(String template) {
+    private static StringReader getTemplate(String template) throws IOException {
         String templateString;
         InputStream templateStream = TemplateFactory.class.getResourceAsStream(template);
-        if (templateStream != null) {
-            try {
-                templateString = IOUtils.toString(templateStream);
-                return new StringReader(templateString);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (templateStream == null) {
+            throw new RuntimeException("Unable to find " + template + " make sure its located in the resources dir");
         }
-        throw new RuntimeException("Unable to find " + template + " make sure its located in the resources dir");
+        templateString = IOUtils.toString(templateStream);
+        return new StringReader(templateString);
     }
 }
