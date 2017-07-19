d47db971e65fe31af3e050c9992bc1f020b51757
nitzanpx <nitzan@perimeterx.com>
2017-07-19 10:11:53 +0300

    added documentaion for the remote config manager interface

--- src/main/java/com/perimeterx/api/remoteconfigurations/RemoteConfigurationManager.java
+++ src/main/java/com/perimeterx/api/remoteconfigurations/RemoteConfigurationManager.java
@@ -1,10 +1,9 @@
 package com.perimeterx.api.remoteconfigurations;
 
-import com.perimeterx.http.PXClient;
 import com.perimeterx.models.configuration.PXDynamicConfiguration;
 
 /**
- * Created by nitzangoldfeder on 19/06/2017.
+ * This interface will be used to retrieve new values for the PXConfiguration
  */
 public interface RemoteConfigurationManager {
     /**
