1bfb11b97e500ecd03c3156e8e5892bcf504caf4
Alex Bluvstein <alex.bluvstein@perimeterx.com>
2018-08-09 16:28:26 +0300

    removed unused S2SCallReason

--- src/main/java/com/perimeterx/models/risk/S2SCallReason.java
+++ src/main/java/com/perimeterx/models/risk/S2SCallReason.java
@@ -12,14 +12,10 @@ public enum S2SCallReason {
 
     NONE("none"),
     NO_COOKIE("no_cookie"),
-    MOBILE_NO_COOKIE("mobile_error_1"),
     COOKIE_EXPIRED("cookie_expired"),
     INVALID_DECRYPTION("cookie_decryption_failed"),
     INVALID_VERIFICATION("cookie_validation_failed"),
-    SENSITIVE_ROUTE("sensitive_route"),
-    MOBILE_SDK_CONNECTION("mobile_error_2"),
-    MOBILE_SDK_PINNING("mobile_error_3"),
-    MOBILE_ERROR_BYPASS("mobile_error_4");
+    SENSITIVE_ROUTE("sensitive_route");
 
     private String value;
 
