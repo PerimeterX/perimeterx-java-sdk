da268bfeba265eba97ddcdeb6f76079d10d1dbdb
Ori Gold <ori.gold@perimeterx.com>
2021-01-20 20:03:20 +0200

    PXS2SValidator: split verify() function and added s2s_error cases

--- src/main/java/com/perimeterx/internals/PXS2SValidator.java
+++ src/main/java/com/perimeterx/internals/PXS2SValidator.java
@@ -8,6 +8,7 @@ import com.perimeterx.models.exceptions.PXException;
 import com.perimeterx.models.httpmodels.RiskResponse;
 import com.perimeterx.models.risk.BlockReason;
 import com.perimeterx.models.risk.PassReason;
+import com.perimeterx.models.risk.S2SErrorReason;
 import com.perimeterx.utils.Constants;
 import com.perimeterx.utils.PXLogger;
 import org.apache.http.conn.ConnectTimeoutException;
@@ -38,7 +39,7 @@ public class PXS2SValidator implements PXValidator {
      */
     public boolean verify(PXContext pxContext) throws PXException {
         logger.debug(PXLogger.LogReason.DEBUG_S2S_RISK_API_REQUEST, pxContext.getS2sCallReason());
-        RiskResponse response;
+        RiskResponse response = null;
         long startRiskRtt = System.currentTimeMillis();
         long rtt;
 
@@ -47,20 +48,12 @@ public class PXS2SValidator implements PXValidator {
             rtt = System.currentTimeMillis() - startRiskRtt;
             logger.debug(PXLogger.LogReason.DEBUG_S2S_RISK_API_RESPONSE, (response == null) ? "" : response.getScore(), rtt);
 
-            pxContext.setMadeS2SApiCall(true);
-            if (response == null) {
-                // Error from PX prepareProxyRequest
-                pxContext.setRiskRtt(rtt);
-                pxContext.setPassReason(PassReason.S2S_ERROR);
+            if (isResponseInvalid(response)) {
+                handleS2SError(pxContext, rtt, response, null);
                 return true;
             }
-            pxContext.setResponsePxhd(response.getPxhd());
-            pxContext.setRiskScore(response.getScore());
-            pxContext.setUuid(response.getUuid());
-            pxContext.setBlockAction(response.getAction());
-            DataEnrichmentCookie dataEnrichment = new DataEnrichmentCookie(response.getDataEnrichment(), true);
-            pxContext.setPxde(dataEnrichment.getJsonPayload());
-            pxContext.setPxdeVerified(dataEnrichment. isValid());
+
+            updateContext(pxContext, response);
 
             if (pxContext.getRiskScore() < pxConfiguration.getBlockingScore()) {
                 pxContext.setPassReason(PassReason.S2S);
@@ -78,11 +71,55 @@ public class PXS2SValidator implements PXValidator {
             pxContext.setPassReason(PassReason.S2S_TIMEOUT);
             return true;
         } catch (Exception e) {
-            pxContext.setRiskRtt(System.currentTimeMillis() - startRiskRtt);
-            pxContext.setPassReason(PassReason.S2S_ERROR);
+            handleS2SError(pxContext, System.currentTimeMillis() - startRiskRtt, response, e);
+            logger.debug("Error {}: {}", e.getMessage(), e.getStackTrace());
             throw new PXException(e);
         } finally {
             pxContext.setRiskRtt(System.currentTimeMillis() - startRiskRtt);
         }
+    }
+
+    private void updateContext(PXContext pxContext, RiskResponse response) {
+        pxContext.setResponsePxhd(response.getPxhd());
+        pxContext.setRiskScore(response.getScore());
+        pxContext.setUuid(response.getUuid());
+        pxContext.setBlockAction(response.getAction());
+        DataEnrichmentCookie dataEnrichment = new DataEnrichmentCookie(response.getDataEnrichment(), true);
+        pxContext.setPxde(dataEnrichment.getJsonPayload());
+        pxContext.setPxdeVerified(dataEnrichment.isValid());
+    }
+
+    private boolean isResponseInvalid(RiskResponse response) {
+        return response == null || response.getStatus() != 0;
+    }
+
+    private void handleS2SError(PXContext pxContext, long rtt, RiskResponse response, Exception exception) {
+        pxContext.setRiskRtt(rtt);
+        pxContext.setPassReason(PassReason.S2S_ERROR);
+
+        if (pxContext.getS2sErrorReason() == S2SErrorReason.NO_ERROR) {
+            S2SErrorReason errorReason = getS2SErrorReason(pxContext, response);
+            String errorMessage = getS2SErrorMessage(response, exception);
+            pxContext.setS2SErrorInfo(errorReason, errorMessage, pxContext.getS2sErrorHttpStatus(), pxContext.getS2sErrorHttpMessage());
+        }
+    }
+
+    private S2SErrorReason getS2SErrorReason(PXContext pxContext, RiskResponse response) {
+        if (!pxContext.isMadeS2SApiCall()) {
+            return S2SErrorReason.UNABLE_TO_SEND_REQUEST;
+        } else if (response != null && isResponseInvalid(response)) {
+            return S2SErrorReason.REQUEST_FAILED_ON_SERVER;
+        }
+        return S2SErrorReason.UNKNOWN_ERROR;
+    }
+
+    private String getS2SErrorMessage(RiskResponse response, Exception exception) {
+        if (exception != null) {
+            return exception.getMessage();
+        } else if (response != null && isResponseInvalid(response)) {
+            return response.getMessage();
+        }
+        int CURRENT_FUNCTION_INDEX = 1;
+        return String.format("Error: %s", Thread.currentThread().getStackTrace()[CURRENT_FUNCTION_INDEX].toString());
     }
 }
