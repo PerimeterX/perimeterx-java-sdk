fe6fd6b4b531f19aac0d191416e44c76e4eec81b
etrpx <elad.trubek@perimeterx.com>
2022-03-22 12:12:39 +0200

    SDKNEW-1766/feature - add constants and utils

---
+++ web/src/main/java/com/web/Constants.java
@@ -0,0 +1,8 @@
+package com.web;
+
+public class Constants {
+    public final static String ENFORCER_CONFIG = "enforcer_config";
+    public final static String PX_USERNAME = "pxUser";
+    public final static String PX_PASSWORD = "1234";
+    public final static String JSON_SUFFIX = ".json";
+}
---
+++ web/src/main/java/com/web/Utils.java
@@ -0,0 +1,68 @@
+package com.web;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static com.web.Constants.ENFORCER_CONFIG;
+import static com.web.Constants.JSON_SUFFIX;
+
+public class Utils {
+    private final static String RESOURCES_RELATIVE_PATH = "src/main/resources/";
+
+    public static void setDefaultPageAttributes(HttpServletRequest request) {
+        final String appId = getAppId();
+        request.setAttribute("app_id", appId);
+        request.setAttribute("sensor_src_url", "/" + getSensorSrc(appId));
+    }
+
+    private static String getAppId() {
+        final String filePath = getEnforcerConfigPath();
+        try {
+            return (String) Objects.requireNonNull(readJsonFile(filePath))
+                    .get("px_app_id");
+        } catch (JsonParseException jpe) {
+            throw new RuntimeException("Failed to extract App ID from file :: " + filePath + ".\n Exception :: " + jpe);
+        }
+    }
+
+    private static String getSensorSrc(String appId) {
+        if (isFirstParty()) {
+            return appId.replace("PX","") + "/init.js";
+        }
+        return String.format( "//client.px-cloud.net/%s/main.min.js",appId);
+    }
+
+    private static boolean isFirstParty() {
+        return (boolean) getEnforcerConfig().get("px_first_party_enabled");
+    }
+
+    private static JSONObject getEnforcerConfig() {
+        final String filePath = getEnforcerConfigPath();
+        try {
+            return readJsonFile(filePath);
+        } catch (JsonParseException jpe) {
+            throw new RuntimeException("Failed to extract config from file :: " + filePath + ".\n Exception :: " + jpe);
+        }
+    }
+
+    private static String getEnforcerConfigPath() {
+        return Thread.currentThread().getContextClassLoader().getResource("").getPath().replace("/target/classes","")
+                + RESOURCES_RELATIVE_PATH + ENFORCER_CONFIG + JSON_SUFFIX;
+    }
+
+    private static JSONObject readJsonFile(String path){
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+            return new JSONObject(new Gson().fromJson(reader, HashMap.class));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
