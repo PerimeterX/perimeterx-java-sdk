889dd0f92d3493dfaca9c42c0c3e92df290e4bb1
etrpx <elad.trubek@perimeterx.com>
2022-04-13 10:40:23 +0300

    SDKNEW-1747/feature - add login details to block and page activities

---
+++ src/main/java/com/perimeterx/models/activities/BaseActivityDetails.java
@@ -0,0 +1,36 @@
+package com.perimeterx.models.activities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.perimeterx.api.additionalContext.AdditionalContext;
+import com.perimeterx.api.additionalContext.credentialsIntelligence.CIProtocol;
+import com.perimeterx.api.additionalContext.credentialsIntelligence.SSOStep;
+import com.perimeterx.models.PXContext;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BaseActivityDetails implements ActivityDetails {
+
+    @JsonProperty("ci_version")
+    public CIProtocol ciProtocol;
+
+    @JsonProperty("sso_step")
+    public SSOStep ssoStep;
+
+    @JsonProperty("credentials_compromised")
+    public boolean credentialsCompromised;
+
+    @JsonProperty("request_id")
+    public UUID requestId;
+
+    public BaseActivityDetails(PXContext context) {
+        final AdditionalContext additionalContext = context.getAdditionalContext();
+        this.ciProtocol = additionalContext.getLoginCredentials().getCiProtocol();
+        this.ssoStep = additionalContext.getLoginCredentials().getSsoStep();
+        this.credentialsCompromised = context.isBreachedAccount();
+        this.requestId = context.getRequestId();
+    }
+}
