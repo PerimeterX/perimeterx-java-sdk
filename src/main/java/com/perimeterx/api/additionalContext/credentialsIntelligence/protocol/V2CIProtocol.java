96558b8974582dfa75a0843635ca7f06d096b139
etrpx <elad.trubek@perimeterx.com>
2022-04-17 10:08:03 +0300

    SDKNEW-1865/bug - not related to PR, refactor V2CIProtocol.java

--- src/main/java/com/perimeterx/api/additionalContext/credentialsIntelligence/protocol/V2CIProtocol.java
+++ src/main/java/com/perimeterx/api/additionalContext/credentialsIntelligence/protocol/V2CIProtocol.java
@@ -17,10 +17,11 @@ public class V2CIProtocol implements CredentialsIntelligenceProtocol {
         final String rawUsername = credentials.getUsername();
         final String normalizedUsername = isValid(rawUsername) ?
                 getV2NormalizedEmailAddress(rawUsername) : rawUsername;
+        final String encodedUsername = encodeString(normalizedUsername, HashAlgorithm.SHA256);
 
         return new UserLoginData(
-                getEncodedV2Password(normalizedUsername, credentials.getPassword()),
-                encodeString(normalizedUsername, HashAlgorithm.SHA256),
+                getEncodedV2Password(encodedUsername, credentials.getPassword()),
+                encodedUsername,
                 rawUsername,
                 CIProtocol.V2,
                 null
@@ -43,8 +44,7 @@ public class V2CIProtocol implements CredentialsIntelligenceProtocol {
         return username + domain;
     }
 
-    private String getEncodedV2Password(String normalizedUsername, String password) {
-        final String encodedUserName = encodeString(normalizedUsername, SHA256);
+    private String getEncodedV2Password(String encodedUserName, String password) {
         final String encodedPassword = encodeString(password, SHA256);
 
         return encodeString(encodedUserName + encodedPassword, SHA256);
