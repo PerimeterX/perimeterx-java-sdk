3d93d211015672822d18bc3310e73673941324c5
guyeisenbach <guy.eisenbach@humansecurity.com>
2023-09-05 19:36:45 +0300

    remove spaces

--- src/main/java/com/perimeterx/http/IPXOutgoingRequest.java
+++ src/main/java/com/perimeterx/http/IPXOutgoingRequest.java
@@ -5,12 +5,7 @@ import java.util.List;
 
 public interface IPXOutgoingRequest {
     String getUrl();
-
     PXHttpMethod getHttpMethod();
-
     PXRequestBody getBody();
-
     List<PXHttpHeader> getHeaders();
-
-
 }
