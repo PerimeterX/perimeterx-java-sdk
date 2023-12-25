31b7ff3e69062faeb6d809e99e7066c5277e08e3
chen-zimmer-px <chen.zimmer@perimeterx.com>
2023-12-25 18:40:08 +0200

    added validation for XHR requests

--- src/main/java/com/perimeterx/api/proxy/DefaultReverseProxy.java
+++ src/main/java/com/perimeterx/api/proxy/DefaultReverseProxy.java
@@ -14,10 +14,12 @@ import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import static com.perimeterx.utils.Constants.CONTENT_TYPE_APPLICATION_JSON;
 import static com.perimeterx.utils.Constants.XHR_PATH;
 import static com.perimeterx.utils.PXResourcesUtil.*;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * Created by nitzangoldfeder on 14/05/2018.
@@ -100,7 +102,18 @@ public class DefaultReverseProxy implements ReverseProxy {
             return true;
         }
 
-        final String url = getPxXhrUrl(pxConfiguration, req.getRequestURI());
+        final String firstPartyAppIdForm = pxConfiguration.getAppId().substring(2);
+        final String xhrPrefix = String.format("/%s/%s", firstPartyAppIdForm, XHR_PATH);
+        final String path = isBlank(req.getRequestURI()) ? "" : req.getRequestURI().substring(xhrPrefix.length());
+        final String url = pxConfiguration.getCollectorUrl() + path;
+        final String host = pxConfiguration.getCollectorUrl().replaceFirst("https?:\\/\\/", "");
+
+        if (!isValidThirdPartyUrl(url, host, path)) {
+            logger.error("First party XHR URL is inaccurate: " + url + ", rendering default response");
+            predefinedResponseHelper.handlePredefinedResponse(res, predefinedResponse);
+            return true;
+        }
+
         final RemoteServer remoteServer = new RemoteServer(url, req, res, ipProvider, proxyClient, predefinedResponse, predefinedResponseHelper, pxConfiguration);
         IPXOutgoingRequest proxyRequest = null;
 
@@ -115,6 +128,18 @@ public class DefaultReverseProxy implements ReverseProxy {
         }
 
         return true;
+    }
+
+    private boolean isValidThirdPartyUrl(String rawThirdPartyUrl, String expectedHost, String expectedUrl) {
+        try {
+            URL url = new URL(rawThirdPartyUrl);
+            String uri = url.getPath() + (url.getQuery() != null ? url.getQuery() : "");
+            return url.getHost().equalsIgnoreCase(expectedHost) && uri.startsWith(expectedUrl);
+        } catch (Exception e) {
+            logger.error("Failed to parse rawUrl. ", e.getMessage());
+        }
+
+        return false;
     }
 
     private void safelyCloseInputStream(IPXOutgoingRequest proxyRequest) throws IOException {
